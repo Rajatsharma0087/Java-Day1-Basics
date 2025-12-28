2cc837da0d2f6de4de856a6f1d7edec9143e68c7
Rajat Sharma <125950478+Rajatsharma0087@users.noreply.github.com>
2025-12-28 15:53:02 +0530

    Rename ConditionExample1 to ConditionIfElse

--- Day2_Conditions_Loops/ConditionIfElse.java
+++ Day2_Conditions_Loops/ConditionIfElse.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class ConditionExample1 {
+public class ConditionIfElse {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
@@ -19,3 +19,4 @@ public class ConditionExample1 {
     }
     
 }
+
